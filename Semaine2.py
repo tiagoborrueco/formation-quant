697af84af1de1096eaa6b356db4323039d3fc1d0
Tiago Borrueco <tiago.borrueco@gmail.com>
2026-06-18 18:56:45 +0200

    semaine 2 : boucles

--- Semaine2.py
+++ Semaine2.py
@@ -1,3 +1,7 @@
+# ========================================
+# =========Variables et f-string==========
+# ========================================
+
 name = "Tiago"
 age = "19"
 
@@ -5,7 +9,9 @@ print(f"My name is {name} and I am {age} years old")
 #f-string is a way to format strings in Python. It allows you to embed expressions inside string literals, using curly braces {}. In this case, the variables name and age are being inserted into the string.
 
 
-# =======================================
+# ========================================
+# ===========Int et String================
+# ========================================
 
 age_str = "19"
 age_int = 19
@@ -15,8 +21,24 @@ print(age_int + age_int) #38
 
 
 # ========================================
-
+# ===========Condtions====================
+# ========================================
 if age_int >= 18:
     print("You are an adult")
 else:
     print("You are a minor")
+
+
+# ========================================
+# =============Boucles====================
+# ========================================
+
+rendement = [0.2, -0.3, -0.04, 0.1, 0.5, -0.2, 0]
+
+for r in rendement:
+    if r < 0:
+        print(f"le rendement est négatif: {r}")
+    elif r > 0:
+        print(f"Le rendement est positif: {r}")
+    else:
+        print(f"Le rendement est nul: {r}")
