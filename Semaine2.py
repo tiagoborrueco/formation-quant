083b80872aef07e586749c513326cb019e77fe06
Tiago Borrueco <tiago.borrueco@gmail.com>
2026-06-18 18:40:26 +0200

    Semaine 2 : conditions

--- Semaine2.py
+++ Semaine2.py
@@ -2,9 +2,21 @@ name = "Tiago"
 age = "19"
 
 print(f"My name is {name} and I am {age} years old")
+#f-string is a way to format strings in Python. It allows you to embed expressions inside string literals, using curly braces {}. In this case, the variables name and age are being inserted into the string.
+
+
+# =======================================
 
 age_str = "19"
 age_int = 19
 
-print(age_str + age_str)
-print(age_int + age_int)
+print(age_str + age_str) #1919
+print(age_int + age_int) #38
+
+
+# ========================================
+
+if age_int >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
